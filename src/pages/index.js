5121261f1491298d95d63d5513522e64a9b639a6
paris-liam <lparis@fordham.edu>
2018-06-12 23:36:14 -0400

    image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Link from 'gatsby-link'
-
+import construction from '../images/construction.png'
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>wait</h1>
+    <img src={construction} alt='construction' />
+    <p>This me, working on my site</p>
+    <p><a href='mailto:paris.liam@gmail.com'>ask me to make your site</a></p>
+    <p><a href='https://twitter.com/liam_paris'>Twitter</a></p>
+    <p><a href='https://www.youtube.com/channel/UCe08QHiiy-nKzeceUDVKpCw'>Videos/Standup</a></p>
+    <p><a href='https://www.youtube.com/channel/UCKw0pHu2fsuAmFIxXBq3mDg'>Sketch</a></p>
+
   </div>
 )
 
