586af24a112cb21b5d5b51a21c31e73434617fc2
paris-liam <lparis@fordham.edu>
2018-09-25 12:22:31 -0400

    removed links

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,27 @@
-import React from 'react'
-import Link from 'gatsby-link'
-import construction from '../images/construction.png'
+import React from 'react';
+import Link from 'gatsby-link';
+import styled from 'styled-components';
+import construction from '../images/construction.png';
+
 const IndexPage = () => (
-  <div>
-    <h1>wait</h1>
-    <img src={construction} alt='construction' />
-    <p>This me, working on my site</p>
-    <p><a href='mailto:paris.liam@gmail.com'>ask me to make your site</a></p>
+  <div style={{
+    display: 'grid',
+    gridTemplateRows: 'auto auto auto',
+    fontFamily: 'sans-serif',
+  }}
+  >
+    <h1>
+      wait
+    </h1>
+    <img src={construction} alt="construction" />
+    <p>
+We're working on it!
+    </p>
+    {/* <p><a href='mailto:paris.liam@gmail.com'>ask me to make your site</a></p>
     <p><a href='https://twitter.com/liam_paris'>Twitter</a></p>
     <p><a href='https://www.youtube.com/channel/UCe08QHiiy-nKzeceUDVKpCw'>Videos/Standup</a></p>
-    <p><a href='https://www.youtube.com/channel/UCKw0pHu2fsuAmFIxXBq3mDg'>Sketch</a></p>
-
+<p><a href='https://www.youtube.com/channel/UCKw0pHu2fsuAmFIxXBq3mDg'>Sketch</a></p> */}
   </div>
-)
+);
 
-export default IndexPage
+export default IndexPage;
