3b8669d63f28a1caeeeb9298f75293360176547c
paris-liam <lparis@fordham.edu>
2018-10-24 14:18:40 -0400

    new

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,19 +8,20 @@ const IndexPage = () => (
     display: 'grid',
     gridTemplateRows: 'auto auto auto',
     fontFamily: 'sans-serif',
+    marginLeft:'2vw',
   }}
   >
     <h1>
-      wait
+      WAIT
     </h1>
     <img src={construction} alt="construction" />
-    <p>
-We're working on it!
-    </p>
-    {/* <p><a href='mailto:paris.liam@gmail.com'>ask me to make your site</a></p>
+    <p>I'm working on it!</p>
+    <h2>In the meantime:</h2>
+    <h3>I make websites and I do comedy</h3>
+    <p><a href='mailto:paris.liam@gmail.com'>ask me to make your site</a></p>
     <p><a href='https://twitter.com/liam_paris'>Twitter</a></p>
     <p><a href='https://www.youtube.com/channel/UCe08QHiiy-nKzeceUDVKpCw'>Videos/Standup</a></p>
-<p><a href='https://www.youtube.com/channel/UCKw0pHu2fsuAmFIxXBq3mDg'>Sketch</a></p> */}
+    <p><a href='https://www.youtube.com/channel/UCKw0pHu2fsuAmFIxXBq3mDg'>Sketch</a></p>
   </div>
 );
 
