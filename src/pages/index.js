9e598e6bddc2def9bcce717eed7cc48dc0d5523f
paris-liam <lparis@fordham.edu>
2018-10-25 11:00:01 -0400

    ok

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,13 +22,6 @@ const IndexPage = () => (
     <p><a href='https://twitter.com/liam_paris'>Twitter</a></p>
     <p><a href='https://www.youtube.com/channel/UCe08QHiiy-nKzeceUDVKpCw'>Videos/Standup</a></p>
     <p><a href='https://www.youtube.com/channel/UCKw0pHu2fsuAmFIxXBq3mDg'>Sketch</a></p>
-    <form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
-      <input type="hidden" name="cmd" value="_s-xclick"/>
-      <input type="hidden" name="hosted_button_id" value="7AKXN7MPNTFJG"/>
-      <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
-      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1" />
-    </form>
-
   </div>
 );
 
