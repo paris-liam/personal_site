ed774cf96bb365a148a7c74ab2c668e0a7131b25
Liam Paris <paris.liam@gmail.com>
2019-06-29 17:55:42 -0400

    added shows

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import styled from 'styled-components';
 import construction from '../images/construction.png';
 
 const IndexPage = () => (
+  <div className='mainGrid'>
   <div style={{
     display: 'grid',
     gridTemplateRows: 'auto auto auto',
@@ -22,6 +23,23 @@ const IndexPage = () => (
     <p><a href='https://twitter.com/liam_paris'>Twitter</a></p>
     <p><a href='https://www.youtube.com/channel/UCe08QHiiy-nKzeceUDVKpCw'>Videos/Standup</a></p>
     <p><a href='https://www.youtube.com/channel/UCKw0pHu2fsuAmFIxXBq3mDg'>Sketch</a></p>
+  </div>
+  <div className='showsColumn'>
+      <h1>
+        Upcoming Shows:
+      </h1>
+      <a href='https://philadelphia.heliumcomedy.com/shows/103612'><p className='showTitle'>Philly's Phunniest round one</p>
+      <p className='showDetails'>7/5 7:30pm at Helium Comedy Club</p></a>
+      <a href='https://goodgoodcomedy.com/setup'><p className='showTitle'>Setup</p>
+      <p className='showDetails'>7/6 7pm at GoodGood</p></a>
+      <a><p className='showTitle'>Everybody Love Me</p>
+      <p className='showDetails'>7/14 6:30pm at Mad Tropical (NYC)</p></a>
+      <a><p className='showTitle'>shots for shots</p>
+      <p className='showDetails'>7/16 9pm at <a>GoodGood</a></p></a>
+      <a href='https://goodgoodcomedy.com/makeuporbreakup072719/'><p className='showTitle'>Makeup or Breakup</p>
+      <p className='showDetails'>7/27 8:30pm at GoodGood</p></a>
+      <a href='https://www.facebook.com/events/2970234833296773'><p className='reoccuringShow'> hosting U UP? open mic @ Fergies every sunday 9pm</p>   </a>  
+  </div>
   </div>
 );
 
