c6fc06937d2077cd1f1980a6f318953b1a19776b
Liam Paris <lparis@fordham.edu>
2019-08-01 14:47:08 -0400

    removed july shows

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,16 +28,8 @@ const IndexPage = () => (
       <h1>
         Upcoming Shows:
       </h1>
-      <a href='https://philadelphia.heliumcomedy.com/shows/103612'><p className='showTitle'>Philly's Phunniest round one</p>
-      <p className='showDetails'>7/5 7:30pm at Helium Comedy Club</p></a>
-      <a href='https://goodgoodcomedy.com/setup'><p className='showTitle'>Setup</p>
-      <p className='showDetails'>7/6 7pm at GoodGood</p></a>
-      <a><p className='showTitle'>Everybody Love Me</p>
-      <p className='showDetails'>7/14 6:30pm at Mad Tropical (NYC)</p></a>
-      <a><p className='showTitle'>shots for shots</p>
-      <p className='showDetails'>7/16 9pm at <a>GoodGood</a></p></a>
-      <a href='https://goodgoodcomedy.com/makeuporbreakup072719/'><p className='showTitle'>Makeup or Breakup</p>
-      <p className='showDetails'>7/27 8:30pm at GoodGood</p></a>
+      <p className='showTitle'>Big Princess @ PHIT 8/3 7:00 </p>
+      <p className='showTitle'>Show and Tell @ PHIT 8/3 10:00 </p>
       <a href='https://www.facebook.com/events/2970234833296773'><p className='reoccuringShow'> hosting U UP? open mic @ Fergies every sunday 9pm</p>   </a>  
   </div>
   </div>
